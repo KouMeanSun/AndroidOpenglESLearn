34917681542d111116a10e6dbfabcc1830679042
gaomingyang <gaomingyang@migu.cn>
2020-08-19 11:43:02 +0800

    first commit

---
+++ app/src/main/java/com/mgsc/opengllearn/MainActivity.java
@@ -0,0 +1,101 @@
+package com.mgsc.opengllearn;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.mgsc.opengllearn.render.FGLViewActivity;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
+    private static final String TAG = "MainActivity";
+    private Context mContext;
+    private RecyclerView mList;
+    private ArrayList<MenuBean> data;
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        // Example of a call to a native method
+//        TextView tv = findViewById(R.id.sample_text);
+//        tv.setText(stringFromJNI());
+        this.commonInit();
+    }
+    private void commonInit(){
+        mContext = this;
+        mList= (RecyclerView)findViewById(R.id.mList);
+        mList.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+        data=new ArrayList<>();
+        add("绘制形体", FGLViewActivity.class);
+        mList.setAdapter(new MenuAdapter());
+    }
+
+    @Override
+    public void onClick(View v) {
+        int position= (int)v.getTag();
+        MenuBean bean=data.get(position);
+        startActivity(new Intent(this,bean.clazz));
+    }
+    private void add(String name,Class<?> clazz){
+        MenuBean bean=new MenuBean();
+        bean.name=name;
+        bean.clazz=clazz;
+        data.add(bean);
+    }
+    private class MenuBean{
+
+        String name;
+        Class<?> clazz;
+
+    }
+    private class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.MenuHolder>{
+
+
+        @Override
+        public MenuHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new MenuHolder(getLayoutInflater().inflate(R.layout.item_button,parent,false));
+        }
+
+        @Override
+        public void onBindViewHolder(MenuHolder holder, int position) {
+            holder.setPosition(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return data.size();
+        }
+
+        class MenuHolder extends RecyclerView.ViewHolder{
+
+            private Button mBtn;
+
+            MenuHolder(View itemView) {
+                super(itemView);
+                mBtn= (Button)itemView.findViewById(R.id.mBtn);
+                mBtn.setOnClickListener(MainActivity.this);
+            }
+
+            public void setPosition(int position){
+                MenuBean bean=data.get(position);
+                mBtn.setText(bean.name);
+                mBtn.setTag(position);
+            }
+        }
+
+    }
+    native String stringFromJNI();
+}
